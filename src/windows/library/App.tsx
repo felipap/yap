a0488204286c18975627b55d61f1e8bbb0bf613c
Felipe Aragão <faragaop@gmail.com>
2026-03-14 20:33:57 -0400

    feat: wip: removing navbar

--- src/windows/library/App.tsx
+++ src/windows/library/App.tsx
@@ -3,7 +3,7 @@ import { useRouter } from '../shared/Router'
 import { onChangeTopLevelPage } from '../shared/ipc'
 import { TopNav } from '../shared/ui/TopNav'
 import LibraryPage from './pages/library'
-import RecordPage from './pages/record'
+import { RecordPage } from './pages/record'
 
 export function App() {
   const { currentRoute } = useRouter()
@@ -17,18 +17,22 @@ export function App() {
   }, [currentRoute.name])
 
   let inner = null
-  if (currentRoute.name === 'library') {
-    inner = <LibraryPage />
-  } else if (currentRoute.name === 'record') {
-    inner = <RecordPage />
+  if (currentRoute.name == 'library' || !currentRoute.name) {
+    inner = (
+      <div>
+        <TopNav currentTab={currentRoute.name} />
+        <div className="flex-1 overflow-hidden">
+          <LibraryPage />
+        </div>
+      </div>
+    )
   } else {
-    inner = <LibraryPage />
+    inner = <RecordPage />
   }
 
   return (
     <div className="flex flex-col h-screen text-[14px] track-10 dark:antialiased">
-      <TopNav currentTab={currentRoute.name} />
-      <div className="flex-1 overflow-hidden">{inner}</div>
+      {inner}
     </div>
   )
 }
